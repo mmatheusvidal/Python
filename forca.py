13dd4f7dfa3da3bb55bb2549c58164ed484db3b1
mmatheusvidal <42753256+mmatheusvidal@users.noreply.github.com>
2019-10-30 12:28:31 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,9 +1,22 @@
+import random
+
 def jogar_forca():
     print('#####################################')
     print('######### Meu Jogo de Forca #########')
     print('#####################################')
 
-    palavra_secreta = 'banana'.upper()
+    arquivo = open('palavras.txt', 'r')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ['_' for a in palavra_secreta]
 
     enforcou = False
